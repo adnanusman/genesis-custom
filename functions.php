f0d7e1be0e2c9e7189879937f5ab62df96b4d82c
Adnan Usman <adnantu@hotmail.com>
2018-02-08 16:40:29 -0800

    Created new style.css and enqueued style from original Genesis Theme

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
 function genesis_sample_google_fonts() {
 
 	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'genesis-css', get_template_directory_uri() . '/style.css', array(), CHILD_THEME_VERSION );
 
 }
 
